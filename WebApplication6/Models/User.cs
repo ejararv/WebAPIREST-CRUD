fe12042bc3d2dd59cdc60e7af54bab54fc8a06cc
ejararv <44954340+ejararv@users.noreply.github.com>
2020-05-07 15:54:13 +0200

    Validation

--- WebApplication6/Models/User.cs
+++ WebApplication6/Models/User.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication6.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Укажите имя пользователя")]//Валидация
         public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Возраст должен быть в промежутке от 1 до 100") ]
+        [Required(ErrorMessage = "Укажите возраст пользователя")]
         public int Age { get; set; }
+
     }
 }
